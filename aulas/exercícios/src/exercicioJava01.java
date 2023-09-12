9ebfb2df697f04086c7fff92c05ec848b4294433
vaguinhu <89532080+vaguinhu@users.noreply.github.com>
2023-09-12 14:25:14 -0300

    Update exercicioJava01.java

--- aulas/exercícios/src/exercicioJava01.java
+++ aulas/exercícios/src/exercicioJava01.java
@@ -1,1 +1,14 @@
+public static void main(String[] args) {
+        // Instancia um objeto da classe Scanner para podermos interagir com o terminal
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite seu nome: ");
+        // Lê a entrada do usuário e associa a variável 'name' do tipo String para usarmos depois
+        String name = scanner.nextLine();
+        System.out.println("O nome digitado é: " + name);
+
+        //Boa prática: Sempre fechar o Scanner
+        scanner.close();
+    }
+
+}
